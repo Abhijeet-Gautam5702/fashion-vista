edb7c6eea662f0e9560fbdb0fb69108d6b0b5b47
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-09-02 22:01:36 +0530

    Write custom classes for API-response & API-Error handling, and create asynController method for controllers

--- backend/src/utilities/customApiError.utils.js
+++ backend/src/utilities/customApiError.utils.js
@@ -1,1 +1,25 @@
-c
+// Cusom API-Error class to handle and standardize the errors thrown by the controllers
+/*
+    NOTE:
+    - CustomApiError class extends the built-in Error class of Node-JS. It has other instance properties beyond the "message" property in the built-in Error class. So CustomApiError class will have access to "message" property of Error class as well as the custom instance properties that we have defined.
+
+    - We want to populate the "message" variable with the message provided as argument in our custom constructor. So we call the constructor of the parent Error-class using "super(message)" with the message property so that the "message" get populated, and populate the other properties manually.
+*/
+class CustomApiError extends Error {
+  // instance properties
+  success;
+  statusCode;
+  data;
+  error;
+
+  // constructor
+  constructor(statusCode, message, error) {
+    super(message);
+    this.statusCode = statusCode;
+    this.error = error;
+    this.success = false;
+    this.data = null;
+  }
+}
+
+export default CustomApiError;
---
+++ backend/src/utilities/customApiResponse.utils.js
@@ -0,0 +1,21 @@
+// Custom API-Response class to standardize the structure in which responses will be sent by the controller functions
+/*
+    NOTE: Wherever required, an instance of the CustomApiResponse class (known as an object) will be created and appropriate message,statusCode,data will be provided
+*/
+class CustomApiResponse {
+  // instance properties
+  data;
+  success;
+  message;
+  statusCode;
+
+  // constructor
+  constructor(message, statusCode, data) {
+    this.success = true;
+    this.data = data;
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default CustomApiResponse;
