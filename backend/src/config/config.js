8db38ed5b4350a163852b4c41113974c34b54591
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-09-06 13:37:29 +0530

    Add cloudinary config variables

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,26 +1,31 @@
-import dotenv from 'dotenv'
-import { DB_NAME } from '../constants.js';
+import dotenv from "dotenv";
+import { DB_NAME } from "../constants.js";
 
 // Config File stores all the configuration related variables of the backend
 const config = {
-    token:{
-        accessToken:{
-            privateKey:process.env.ACCESS_TOKEN_PRIVATE_KEY,
-            expiry:process.env.ACCESS_TOKEN_EXPIRY,
-        },
-        refreshToken:{
-            privateKey:process.env.REFRESH_TOKEN_PRIVATE_KEY,
-            expiry:process.env.REFRESH_TOKEN_EXPIRY,
-        },
+  token: {
+    accessToken: {
+      privateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY,
+      expiry: process.env.ACCESS_TOKEN_EXPIRY,
     },
-    database:{
-        mongoDBConnectionString:process.env.MONGO_DB_CONNECTION_STRING,
-        databaseName:DB_NAME,
+    refreshToken: {
+      privateKey: process.env.REFRESH_TOKEN_PRIVATE_KEY,
+      expiry: process.env.REFRESH_TOKEN_EXPIRY,
     },
-    app:{
-        port:process.env.PORT || 8000,
-        corsOrigin:process.env.CORS_ORIGIN || "*",
-    }
-}
+  },
+  database: {
+    mongoDBConnectionString: process.env.MONGO_DB_CONNECTION_STRING,
+    databaseName: DB_NAME,
+  },
+  app: {
+    port: process.env.PORT || 8000,
+    corsOrigin: process.env.CORS_ORIGIN || "*",
+  },
+  cloudinary: {
+    apiKey: process.env.CLOUDINARY_API_KEY,
+    apiSecret: process.env.CLOUDINARY_API_SECRET,
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  },
+};
 
 export default config;
