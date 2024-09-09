09d3e45ae70490e9715a382e048c4f8d5f264ed8
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-09-09 23:08:19 +0530

    Modify tailwind class to ensure Container occupies the available space

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -1,5 +1,9 @@
 function Container({ children }) {
-  return <div>{children}</div>;
+  return (
+    <div className="flex-grow flex flex-col justify-center items-center w-full">
+      {children}
+    </div>
+  );
 }
 
 export default Container;
