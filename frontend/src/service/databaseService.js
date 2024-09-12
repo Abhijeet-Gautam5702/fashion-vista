7d2a6c882ac4530dec1dbba162631d6b9e5a791e
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-09-12 20:45:48 +0530

    Add new database service method to fetch cart details of the current logged-in user

--- frontend/src/service/databaseService.js
+++ frontend/src/service/databaseService.js
@@ -73,6 +73,28 @@ class DatabaseService {
   async updateProductQtInCart() {}
 
   async removeProductFromCart() {}
+
+  async getUserCart() {
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/api/v1/cart/get-cart`,
+        { withCredentials: true }
+        /*
+          NOTE: For Axios Requests, this setting must be set to true.
+          `withCredentials:true` option enables the browser to send cookies and other credential-related stuff to the server (located in a different domain than the frontend-client)
+        */
+      );
+      return response.data;
+    } catch (error) {
+      /*
+        AXIOS ERROR HANDLING
+        The error sent by the backend server is stored inside the `AxiosError.response.data` object
+
+        NOTE: We will send the exact same error object (sent by the backend service) to the client. Therefore, we throw `error.response.data` instead of `error` simply.
+      */
+      throw error.response.data;
+    }
+  }
 }
 
 const databaseService = new DatabaseService();
