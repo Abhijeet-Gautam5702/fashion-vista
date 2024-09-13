6871d29b58ab9e2f4a74fba0c54ba37881c1b273
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-09-13 20:09:39 +0530

    Add reducers to populate the store with orders and add an order item to the slice

--- frontend/src/store/orderSlice/orderSlice.js
+++ frontend/src/store/orderSlice/orderSlice.js
@@ -1,19 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  order:[]
+  orders: [],
 };
 
 const orderSlice = createSlice({
   name: "order", // to be used when registering in the store
   initialState,
   reducers: {
-    storePopulateOrders: (state, action) => {},
+    storePopulateOrders: (state, action) => {
+      state.orders=action.payload.orders
+    },
+    storeAddOrder: (state, action) => {
+      state.orders.push(action.payload.order);
+    },
     storeCancelOrder: (state, action) => {}, // only if the status of the order is not "shipped" or "out for delivery" or "delivered"
   },
 });
 // export orderSlice reducers individually
-export const { storePopulateOrders, storeCancelOrder } = orderSlice.actions;
+export const {storeAddOrder, storePopulateOrders, storeCancelOrder } = orderSlice.actions;
 
 // export all reducers as a single orderReducer and register it in the store
 export const orderReducer = orderSlice.reducer;
