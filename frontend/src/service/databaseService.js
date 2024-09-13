090b691b7c717517459b03df79b1ef9f254fdca3
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-09-13 16:36:23 +0530

    Write database services for product removal and updation in cart

--- frontend/src/service/databaseService.js
+++ frontend/src/service/databaseService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 class DatabaseService {
+  // PRODUCT RELATED ENDPOINTS
   async getAllProducts() {
     try {
       const response = await axios.get(
@@ -45,6 +46,7 @@ class DatabaseService {
     }
   }
 
+  // CART RELATED ENDPOINTS
   async addProductToCart(data) {
     try {
       const response = await axios.post(
@@ -70,14 +72,56 @@ class DatabaseService {
     }
   }
 
-  async updateProductQtInCart() {}
-
-  async removeProductFromCart() {}
-
   async getUserCart() {
     try {
       const response = await axios.get(
         `http://localhost:8000/api/v1/cart/get-cart`,
+        { withCredentials: true }
+        /*
+          NOTE: For Axios Requests, this setting must be set to true.
+          `withCredentials:true` option enables the browser to send cookies and other credential-related stuff to the server (located in a different domain than the frontend-client)
+        */
+      );
+      return response.data;
+    } catch (error) {
+      /*
+        AXIOS ERROR HANDLING
+        The error sent by the backend server is stored inside the `AxiosError.response.data` object
+
+        NOTE: We will send the exact same error object (sent by the backend service) to the client. Therefore, we throw `error.response.data` instead of `error` simply.
+      */
+      throw error.response.data;
+    }
+  }
+
+  async removeProductFromCart(productId, size) {
+    try {
+      const response = await axios.put(
+        `http://localhost:8000/api/v1/products/remove-from-cart?productId=${productId}&size=${size}`,
+        {},
+        { withCredentials: true }
+        /*
+          NOTE: For Axios Requests, this setting must be set to true.
+          `withCredentials:true` option enables the browser to send cookies and other credential-related stuff to the server (located in a different domain than the frontend-client)
+        */
+      );
+      return response.data;
+    } catch (error) {
+      /*
+        AXIOS ERROR HANDLING
+        The error sent by the backend server is stored inside the `AxiosError.response.data` object
+
+        NOTE: We will send the exact same error object (sent by the backend service) to the client. Therefore, we throw `error.response.data` instead of `error` simply.
+      */
+      throw error.response.data;
+    }
+  }
+
+  async updateProductQtInCart(productId, size, quantity) {
+    try {
+      const response = await axios.put(
+        `http://localhost:8000/api/v1/products/update-product-qt-in-cart?productId=${productId}&size=${size}&quantity=${quantity}`,
+        {},
         { withCredentials: true }
         /*
           NOTE: For Axios Requests, this setting must be set to true.
