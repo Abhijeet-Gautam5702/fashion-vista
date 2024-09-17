c72967ea6114578e19f5363cc580e2752f044dfe
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-09-17 11:12:45 +0530

    Minor UI changes in Admin Header

--- frontend/src/components/Header/AdminHeader.jsx
+++ frontend/src/components/Header/AdminHeader.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { authService } from "../../service";
 import { useDispatch } from "react-redux";
 import { storeAdminLogout, storeLogout } from "../../store/authSlice/authSlice";
+import toast from "react-hot-toast";
 
 function AdminHeader() {
   const navigate = useNavigate();
@@ -17,6 +18,17 @@ function AdminHeader() {
         dispatch(storeAdminLogout());
         // navigate to admin login page
         navigate("/admin/", { replace: true });
+        // show toast
+        toast("ADMIN LOGGED OUT SUCCESSFULLY", {
+          duration: 1500,
+          position: "top-center",
+          icon: "✅",
+          style: {
+            fontFamily: "Outfit",
+            fontWeight: "500",
+            fontSize: "14px",
+          },
+        });
       }
     } catch (error) {
       console.log(`Admin Logout Failed | Error = ${error.message}`);
@@ -28,12 +40,12 @@ function AdminHeader() {
     <div className="px-3 border-y-[1.5px] border-y-gray flex-grow-0 w-full flex flex-row justify-between items-center py-4">
       {/* Logo */}
       <Link to="/admin/inventory">
-        <Logo className={"text-size-30 text-black-2"} />
+        <Logo className={"text-size-24 text-black-2"} />
       </Link>
       {/* Logout Button */}
       <button
         type="button"
-        className="py-2 px-5 bg-white border-[1.5px] border-black text-black hover:bg-black hover:text-white transition-colors duration-150 cursor-pointer font-main text-size-14 font-500"
+        className="py-2 px-5 bg-white border-[1.5px] border-black text-black hover:bg-black hover:text-white transition-colors duration-150 cursor-pointer font-main text-size-12 font-500 rounded-full"
         onClick={adminLogoutHandler}
       >
         Logout
