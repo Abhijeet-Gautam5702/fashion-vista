6828b41605646865379f5189a0aaa4ff4d4af4bd
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-09-20 16:46:33 +0530

    Add responsiveness to About and Contact Page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -17,9 +17,9 @@ function About() {
         ABOUT US
       </p>
       {/* Hero */}
-      <div className="w-full flex flex-row justify-between items-center gap-16">
+      <div className="w-full flex flex-col lg:flex-row justify-start lg:justify-between items-center gap-16">
         {/* Image */}
-        <div className="w-1/2 flex flex-row items-center justify-center flex-grow">
+        <div className="w-full lg:w-1/2 flex flex-row items-center justify-center flex-grow">
           <img
             className="w-full flex-grow text-center font-main text-black-1 font-400 text-size-12 py-auto"
             src={banner_2}
@@ -27,7 +27,7 @@ function About() {
           />
         </div>
         {/* About Text */}
-        <div className="w-1/2 flex flex-col justify-start items-start gap-6 font-main font-400 text-text-col-2 text-size-17">
+        <div className="w-full lg:w-1/2 flex flex-col justify-start items-start gap-6 font-main font-400 text-text-col-2 text-size-17">
           <p>
             Fashion Vista was born from a passion for innovation and a
             commitment to transforming the online shopping experience. Our
@@ -54,31 +54,31 @@ function About() {
           </div>
         </div>
       </div>
-      {/* Why Choose Us */}
+      {/* Why Do We Stand Out */}
       <div className="w-full flex flex-col items-center justify-start gap-7 font-main">
-        <p className="w-full text-left font-500 text-size-20 text-text-col-2">
+        <p className="w-full text-center lg:text-left font-500 text-size-20 text-text-col-2">
           WHY DO WE STAND OUT?
         </p>
-        <div className="w-full flex flex-row justify-between items-stretch">
-          <div className="w-1/3 border-[1px] px-10 py-16 text-size-14 text-text-col-2 font-400 gap-3 border-gray flex flex-col items-center justify-center">
-            <p className="font-500 text-black text-size-16">
+        <div className="w-full flex flex-col sm:flex-row justify-start sm:justify-between items-stretch">
+          <div className=" w-full sm:w-1/3 border-[1px] px-10 py-12 md:py-8 sm:py-16 text-size-14 text-text-col-2 font-400 gap-3 border-gray flex flex-col items-center justify-start">
+            <p className="w-full text-left font-500 text-black text-size-16">
               Quality Assurance
             </p>
-            <p className="font-400">
+            <p className="font-400 text-size-12">
               We meticulously select and vet each product to ensure it meets our
               stringent quality standards.
             </p>
           </div>
-          <div className="w-1/3 border-[1px] px-10 py-16 text-size-14 text-text-col-2 font-400 gap-3 border-gray flex flex-col items-center justify-center">
-            <p className="font-500 text-black text-size-16">Convenience</p>
-            <p className="font-400">
+          <div className=" w-full sm:w-1/3 border-[1px] px-10 py-12 md:py-8 sm:py-16 text-size-14 text-text-col-2 font-400 gap-3 border-gray flex flex-col items-center justify-start">
+            <p className="w-full text-left font-500 text-black text-size-16">Convenience</p>
+            <p className="font-400 text-size-12">
               With our user-friendly interface and hassle-free ordering process,
               shopping has never been easier.
             </p>
           </div>
-          <div className="w-1/3 border-[1px] px-10 py-16 text-size-14 text-text-col-2 font-400 gap-3 border-gray flex flex-col items-center justify-center">
-            <p className="font-500 text-black text-size-16">Customer Service</p>
-            <p className="font-400">
+          <div className=" w-full sm:w-1/3 border-[1px] px-10 py-12 md:py-8 sm:py-16 text-size-14 text-text-col-2 font-400 gap-3 border-gray flex flex-col items-center justify-start">
+            <p className="w-full text-left font-500 text-black text-size-16">Customer Service</p>
+            <p className="font-400 text-size-12">
               Our team of dedicated professionals is here to assist you the way,
               ensuring your satisfaction is our top priority.
             </p>
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { banner_2 ,contact} from "../assets";
+import { banner_2, contact } from "../assets";
 import { Button } from "../components";
 
 function Contact() {
@@ -12,13 +12,13 @@ function Contact() {
   }, []);
 
   return (
-    <div className="border-y-[1.5px] border-y-gray pt-8 pb-16 w-full flex flex-col justify-start items-center gap-12">
+    <div className="border-y-[1.5px] border-y-gray pt-8 pb-16 flex-grow w-full flex flex-col justify-center items-center gap-12">
       <p className="w-full text-center font-main text-text-col-2 text-size-24 font-500">
         CONTACT US
       </p>
-      <div className="w-full flex flex-row items-center justify-between font-main px-10 gap-10">
+      <div className="w-full flex flex-col md:flex-row items-center justify-between font-main px-10 gap-10">
         {/* Image */}
-        <div className="w-1/2 flex flex-row items-center justify-center flex-grow">
+        <div className="w-full md:w-1/2 flex flex-row items-center justify-center flex-grow">
           <img
             className="w-full flex-grow text-center font-main text-black-1 font-400 text-size-12 py-auto"
             src={contact}
@@ -26,9 +26,9 @@ function Contact() {
           />
         </div>
         {/* Contact details */}
-        <div className="w-1/2 flex flex-col justify-center items-start gap-8 leading-normal text-text-col-1 font-500 text-size-16">
-          <div className="flex flex-col justify-start items-start gap-1 text-size-16 text-text-col-1 font-500">
-            <p className="font-600 text-text-col-2 text-size-20 mb-1">
+        <div className="w-full md:w-1/2 flex flex-col justify-center items-start gap-8 leading-normal text-text-col-1 font-500">
+          <div className="flex flex-col justify-start items-start gap-1 text-size-12 md:text-size-16 text-text-col-1 font-500">
+            <p className="font-600 text-text-col-2 text-size-14 md:text-size-20 mb-1">
               {" "}
               Shop from us offline at
             </p>
@@ -36,20 +36,20 @@ function Contact() {
             <p>Washington DC, USA</p>
           </div>
 
-          <div className="flex flex-col justify-start items-start gap-1 text-size-16 text-text-col-1 font-500">
-            <p className="text-size-17">Email: abhidevelops572@gmail.com</p>
-            <p className="text-size-17">Phone: +91 4561238922</p>
+          <div className="flex flex-col justify-start items-start gap-1 text-size-12 md:text-size-16 text-text-col-1 font-500">
+            <p>Email: abhidevelops572@gmail.com</p>
+            <p>Phone: +91 4561238922</p>
           </div>
 
-          <div className="flex flex-col justify-start items-start gap-1 text-size-16 text-text-col-1 font-500">
-            <p className="font-600 text-text-col-2 text-size-20 mb-1">
+          <div className="flex flex-col justify-start items-start gap-1 text-size-12 md:text-size-16 text-text-col-1 font-500">
+            <p className="font-600 text-text-col-2 text-size-14 md:text-size-20 mb-1">
               {" "}
               Careers at{" "}
               <span className="font-logo leading-none">Fashion Vista</span>
             </p>
             <p>St. Petersburg Street</p>
             <p>Washington DC, USA</p>
-            <button className="mt-6 bg-white text-black hover:bg-black hover:text-white px-8 py-4 border-[1.5px] border-black transition-colors duration-150">
+            <button className="mt-6 bg-white text-black hover:bg-black hover:text-white px-8 py-3 md:py-4 border-[1.5px] border-black transition-colors duration-150">
               Explore careers
             </button>
           </div>
